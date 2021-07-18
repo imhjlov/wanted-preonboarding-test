74ab2b40c89b0fa29028ab5567d384913c103cfc
hj <wprkftngh@gmail.com>
2021-07-19 01:17:06 +0900

    [feat]dropdown메뉴 데이터 추가

---
+++ src/data/NavSubMenuData.js
@@ -0,0 +1,240 @@
+import React from "react";
+
+export const NavSubMenuData = [
+  {
+    title: "영업",
+    path: "/",
+
+    subExplore: [
+      {
+        title: "기업영업",
+        path: "/",
+      },
+      {
+        title: "외부영업",
+        path: "/",
+      },
+      {
+        title: "영업 관리자",
+        path: "/",
+      },
+      {
+        title: "기술영업",
+        path: "/",
+      },
+      {
+        title: "주요고객사 담당자",
+        path: "/",
+      },
+      {
+        title: "솔루션 컨설턴트",
+        path: "/",
+      },
+      {
+        title: "해외영업",
+        path: "/",
+      },
+      {
+        title: "고객성공매니저",
+        path: "/",
+      },
+    ],
+  },
+  {
+    title: "미디어",
+    path: "/",
+    subExplore: [
+      {
+        title: "콘텐츠 크리에이터",
+        path: "/",
+      },
+      {
+        title: "PD",
+        path: "/",
+      },
+      {
+        title: "영상 편집가",
+        path: "/",
+      },
+      {
+        title: "에디터",
+        path: "/",
+      },
+      {
+        title: "비디오 제작",
+        path: "/",
+      },
+      {
+        title: "작가",
+        path: "/",
+      },
+      {
+        title: "출판 기획자",
+        path: "/",
+      },
+      {
+        title: "통·번역",
+        path: "/",
+      },
+    ],
+  },
+  {
+    title: "인사",
+    path: "/",
+    subExplore: [
+      {
+        title: "인사담당",
+        path: "/",
+      },
+      {
+        title: "리크루터",
+        path: "/",
+      },
+      {
+        title: "조직문화",
+        path: "/",
+      },
+      {
+        title: "평가·보상",
+        path: "/",
+      },
+      {
+        title: "헤드헌터",
+        path: "/",
+      },
+      {
+        title: "HRD",
+        path: "/",
+      },
+      {
+        title: "HRBP",
+        path: "/",
+      },
+      {
+        title: "급여담당",
+        path: "/",
+      },
+    ],
+  },
+  {
+    title: "게임 제작",
+    path: "/",
+    subExplore: [
+      {
+        title: "게임 기획자",
+        path: "/",
+      },
+      {
+        title: "게임 그래픽 디자이너",
+        path: "/",
+      },
+      {
+        title: "모바일 게임 개발자",
+        path: "/",
+      },
+      {
+        title: "게임 클라이언트 개발자",
+        path: "/",
+      },
+      {
+        title: "게임 아티스트",
+        path: "/",
+      },
+      {
+        title: "유니티 개발자",
+        path: "/",
+      },
+      {
+        title: "게임 서버 개발자",
+        path: "/",
+      },
+      {
+        title: "언리얼 개발자",
+        path: "/",
+      },
+    ],
+  },
+  {
+    title: "금융",
+    path: "/",
+    subExplore: [
+      {
+        title: "회계담당",
+        path: "/",
+      },
+      {
+        title: "재무 담당자",
+        path: "/",
+      },
+      {
+        title: "IR",
+        path: "/",
+      },
+      {
+        title: "투자·증권",
+        path: "/",
+      },
+      {
+        title: "재무 분석가",
+        path: "/",
+      },
+      {
+        title: "애널리스트",
+        path: "/",
+      },
+      {
+        title: "내부통제 담당자",
+        path: "/",
+      },
+      {
+        title: "부동산 자산 관리자",
+        path: "/",
+      },
+    ],
+  },
+  {
+    title: "엔지니어링·설계",
+    path: "/",
+    subExplore: { title: "" },
+  },
+  {
+    title: "물류·무역",
+    path: "/",
+    subExplore: { title: "" },
+  },
+  {
+    title: "제조·생산",
+    path: "/",
+    subExplore: { title: "" },
+  },
+  {
+    title: "의료·제약·바이오",
+    path: "/",
+    subExplore: { title: "" },
+  },
+  {
+    title: "교육",
+    path: "/",
+    subExplore: { title: "" },
+  },
+  {
+    title: "법률·법집행기관",
+    path: "/",
+    subExplore: { title: "" },
+  },
+  {
+    title: "식·음료",
+    path: "/",
+    subExplore: { title: "" },
+  },
+  {
+    title: "건설·시설",
+    path: "/",
+    subExplore: { title: "" },
+  },
+  {
+    title: "공공·복지",
+    path: "/",
+    subExplore: { title: "" },
+  },
+];
